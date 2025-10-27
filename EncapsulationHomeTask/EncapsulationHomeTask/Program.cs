b936c1f1cfea4b75c3daf6366fd337c99dfc6aa6
BrahmsYT <qasimlirenat@gmail.com>
2025-10-28 02:53:37 +0400

    Task still not ready yet

---
+++ EncapsulationHomeTask/EncapsulationHomeTask/Program.cs
@@ -0,0 +1,76 @@
+﻿using EncapsulationHomeTask;
+using System.Xml.Serialization;
+
+User user = new User();
+user.SetName();
+user.SetSurname();
+user.SetEmail();
+user.SetAge();
+user.PassChecker();
+user.AccessPassword();
+
+User user1 = new User();
+user1.SetName();
+user1.SetSurname();
+user1.SetEmail();
+user1.SetAge();
+user1.PassChecker();
+user1.AccessPassword();
+
+User user2 = new User();
+user2.SetName();
+user2.SetSurname();
+user2.SetEmail();
+user2.SetAge();
+user2.PassChecker();
+user2.AccessPassword();
+
+User user3 = new User();
+user3.SetName();
+user3.SetSurname();
+user3.SetEmail();
+user3.SetAge();
+user3.PassChecker();
+user3.AccessPassword();
+
+
+string coice = Console.ReadLine();
+while (true)
+{
+    if (coice == "1")
+    {  
+        User[] users = { user , user1 , user2 , user3  };
+        foreach (var usr in users)
+        {
+            usr.GetInfo();
+            Console.WriteLine("===================================");
+        }
+    }
+    else if (coice == "2")
+    {
+        int idToFind;
+        Console.Write("Enter the ID of the user to find: ");
+        idToFind = int.Parse(Console.ReadLine()) ;
+        User[] users = { user , user1 , user2 , user3  };
+        foreach (var usr in users)
+        {
+            if (usr.ID == idToFind)
+            {
+                usr.GetInfo();
+                break;
+            }
+        }
+    }
+    else if (coice == "3")
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice. Please try again.");
+        Console.WriteLine("Enter 1 to display all users, 2 to find a user by ID, or 3 to exit:");
+        Console.ReadLine();
+    }
+
+
+}
---
+++ EncapsulationHomeTask/EncapsulationHomeTask/User.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace EncapsulationHomeTask
+{
+    internal class User
+    {
+        private static int _lastId;
+        public int ID { get; private set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Password { get; private set; }
+
+        public User(string name, string surname, int age, string email, string password = null)
+        {
+            ID = Interlocked.Increment(ref _lastId);
+            Name = name;
+            Surname = surname;
+            Age = age;
+            Email = email;
+            if (!string.IsNullOrEmpty(password))
+                Password = password;
+        }
+
+        public User()
+        {
+            ID = Interlocked.Increment(ref _lastId);
+        }
+
+        public void PassChecker()
+        {
+            while (true)
+            {
+                Console.Write("Please enter your password: ");
+                string inputPassword = Console.ReadLine() ?? string.Empty;
+
+                if (inputPassword.Length >= 8
+                    && inputPassword.Any(char.IsUpper)
+                    && inputPassword.Any(char.IsDigit)
+                    && inputPassword.Any(char.IsLower))
+                {
+                    Password = inputPassword;
+                    break;
+                }
+
+                Console.WriteLine("Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, and one digit. Try again.");
+            }
+        }
+
+        public void AccessPassword()
+        {
+            while (true)
+            {
+                Console.Write("Please re-enter your password: ");
+                string inputPassword = Console.ReadLine() ?? string.Empty;
+
+                if (inputPassword == Password)
+                {
+                    Console.WriteLine("Access granted.");
+                    break;
+                }
+
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Surname: {Surname}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Email: {Email}");
+        }
+        public void SetName()
+        {
+            Console.Write("Enter new name: ");
+            string newName = Console.ReadLine() ?? string.Empty;
+            Name = newName;
+        }
+
+        public void SetEmail()
+        {
+            Console.Write("Enter new email: ");
+            string newEmail = Console.ReadLine() ?? string.Empty;
+            Email = newEmail;
+        }
+
+        public void SetSurname()
+        {
+            Console.Write("Enter new surname: ");
+            string newSurname = Console.ReadLine() ?? string.Empty;
+            Surname = newSurname;
+
+        }
+
+        public void SetAge()
+        {
+            Console.Write("Enter new age: ");
+            string newAge = Console.ReadLine() ?? string.Empty;
+            Age = int.Parse(newAge);
+        }
+    }
+}
